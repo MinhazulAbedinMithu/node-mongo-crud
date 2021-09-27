e3fd6dbcb681ebb6ef1ec32ae783cdd96d1e475c
MinhazulAbedinMithu <minhazulabedinmithu@gmail.com>
2021-09-27 21:19:31 +0600

    re write

--- index.js
+++ index.js
@@ -1,76 +1,72 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const MongoClient = require('mongodb').MongoClient;
-const ObjectId = require('mongodb').ObjectId
-
-const uri = "mongodb+srv://organicUser:y!ebKqijdNvY.4L@cluster0.onh2u.mongodb.net/organicdb?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-
+const express = require("express");
+const { MongoClient, ObjectId } = require("mongodb");
+const bodyParser = require("body-parser");
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
-})
 
-
-
-client.connect(err => {
-  const productCollection = client.db("organicdb").collection("products");
-  //get product collection
-  app.get('/products', (req, res) => {
-    productCollection.find({})
-    .toArray((err, documents) => {
-      res.send(documents);
-    })
-  })
-
-  //post product collection
-  app.post("/addProduct", (req, res) => {
-    const product = req.body;
-    console.log(product);
-    productCollection.insertOne(product)
-    .then(result => {
-        console.log('One product added');
-        res.redirect('/');
-    });
-  })
-  
-  //delete product collection
-  app.delete('/delete/:id', (req, res) => {
-    const id = req.params.id;
-    productCollection.deleteOne({_id: ObjectId(id)})
-    .then((result) => {
-      res.send(result.deletedCount > 0);
-    })
-  })
-
-  app.get('/product/:id', (req, res) => {
-    const id = req.params.id;
-    productCollection.find({_id: ObjectId(id)})
-    .toArray((err, documents) => {
-      res.send(documents[0]);
-    })
-  })
-
-  app.patch('/update/:id', (req, res) => {
-    const id = req.params.id;
-    productCollection.updateOne({_id: ObjectId(id)},
-    {
-      $set: {price: req.body.price, quantity: req.body.quantity}
-    })
-    .then(result => {
-      res.send(result.modifiedCount > 0);
-    })
-
-  })
-
-  
-  
-  console.log('database created')
-  // client.close();
+//send file in localhost::::::
+app.get("/", (req, res) => {
+	res.sendFile(__dirname + "/index.html");
 });
- 
 
-app.listen(3000); 
+//Database Connections
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.onh2u.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://root:toor@cluster0.onh2u.mongodb.net/milestone-10?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
+client.connect((err) => {
+	const collection = client.db("milestone-10").collection("node-mongo-crud");
+
+	//insert data in MongoDB
+	app.post("/addItem", (req, res) => {
+		collection.insertOne(req.body).then((result) => {
+			res.send("Success");
+		});
+	});
+
+	app.get("/items", (req, res) => {
+		collection.find({}).toArray((err, docs) => {
+			res.send(docs);
+			console.log("Items Render Success");
+		});
+	});
+
+	app.get("/item/:id", (req, res) => {
+		collection.find({ _id: ObjectId(req.params.id) }).toArray((err, docs) => {
+			res.send(docs);
+		});
+	});
+
+	app.patch("/update/:id", (req, res) => {
+		collection.updateOne({ _id: ObjectId(req.params.id)},{
+					$set: {
+						age: req.body.updatedAge,
+						study: req.body.updatedStudy,
+					},
+				}
+			)
+			.then((result) => {
+				res.send(result.modifiedCount > 0);
+			});
+	});
+
+	app.delete("/delete/:id", (req, res) => {
+		collection.deleteOne({ _id: ObjectId(req.params.id) })
+    .then((result) => {
+				res.send(result.DeletedCount > 0);
+			});
+	});
+
+	console.log("DB connected.");
+});
+
+//define PORT number and app listening:::
+const PORT = 5000;
+app.listen(PORT, (err, res) => {
+	console.log(`App Listening at ${PORT}`);
+});
